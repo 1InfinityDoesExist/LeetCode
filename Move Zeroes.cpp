bdc90e159ebc6123aaf3b36ba367d887a73bfb95
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-05-29 10:57:24 +0530

    Add files via upload

--- Move Zeroes.cpp
+++ Move Zeroes.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int ct = count(nums.begin(), nums.end(), 0);
-
+        int len = nums.size();
         nums.erase(remove(nums.begin(), nums.end(), 0), nums.end());
-        while(ct--)
+        int new_len = nums.size();
+        int diff = len - new_len;
+        while(diff--)
         {
             nums.push_back(0);
         }
-        return;
-
+        for(vector<int>::iterator iter = nums.begin(); iter != nums.end(); iter++)
+        {
+            cout << *iter << " ";
+        }
     }
 };
