7e79e0c49714b217e7bb7f6e88f6c48fe5544b55
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-08 22:55:34 +0530

    Add files via upload

--- Unique Binary Search Trees.cpp
+++ Unique Binary Search Trees.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int numTrees(int n) {
-        long long int *dp = (long long int *)malloc(sizeof(long long int) * n+1);
+        int *dp = (int *)malloc(sizeof(int) * n+1);
+        for(int iter = 0; iter <= n; iter++)
+        {
+            dp[iter] = 0;
+        }
         dp[0] = dp[1] = 1;
         for(int iter = 2; iter <= n; iter++)
         {
-            for(int jter = 1; jter <= iter; jter++)
+            for(int jter = 1; jter <= iter/2; jter++)
             {
                 dp[iter] += dp[jter-1]*dp[iter-jter];
             }
+            dp[iter] *= 2;
+            if(iter%2)
+            {
+                dp[iter] += dp[iter/2]*dp[iter/2];
+            }
+
         }
-        long long int ans = dp[n];
+        int ans = dp[n];
         free(dp);
         return ans;
     }
