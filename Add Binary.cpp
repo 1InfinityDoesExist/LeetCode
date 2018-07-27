f243310015d08375ecdf3302b8e971e184d452ca
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-07-27 20:04:55 +0530

    Add files via upload

--- Add Binary.cpp
+++ Add Binary.cpp
@@ -1,17 +1,53 @@
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        string str = "";
-        int c = 0;
-        int iter = a.size()-1;
-        int jter = b.size()-1;
-        while(iter >= 0 || jter >= 0 || c == 1)
+    string addBinary(string a, string b)
+    {
+
+        int mx = max(a.length(), b.length());
+        if(mx > a.length())
         {
-            c += iter >= 0 ? a[iter--] - '0' : 0;
-            c += jter >= 0 ? b[jter--] - '0' : 0;
-            str = char(c%2 + '0') + str;
-            c /= 2;
+            a = string(mx-a.length(), '0') + a;
         }
-        return str;
+        if(mx > b.length())
+        {
+            b = string(mx - b.length(), '0') + b;
+        }
+        string final(mx+1, '0');
+        string::const_reverse_iterator iter = a.rbegin(), s = a.rend();
+        string::const_reverse_iterator jter = b.rbegin();
+        string::reverse_iterator fter = final.rbegin();
+        char carry = 0;
+        for(; iter != s; iter++, jter++, fter++)
+        {
+            char temp = (*iter - '0') + (*jter - '0') + carry;
+            if(temp == 0)
+            {
+                carry = 0;
+            }
+            else if(temp == 1)
+            {
+                carry = 0;
+            }
+            else if(temp == 2)
+            {
+                carry = 1;
+                temp = 0;
+            }
+            else if(temp == 3)
+            {
+                carry = 1;
+                temp = 1;
+            }
+            *fter = temp + '0';
+        }
+        if(carry == 1)
+        {
+            final[0] = carry + '0';
+        }
+        else
+        {
+            return final.substr(1, final.length()-1);
+        }
+        return final;
     }
 };
